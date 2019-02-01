bb85932a258492ee5be15297288f9c80eb2f8913
Wilson <wilsonj806@gmail.com>
2019-02-01 11:34:06 -0500

    Add <Form/>, add OAuth resources
    - Added <For/> which is taken from an old tutorial
    - Added OAuth resources for later

--- doc/oauth-resources.md
+++ doc/oauth-resources.md
@@ -14,3 +14,25 @@ This is the general standdard on how your app consumes external APIs. NOTE THIS 
 - https://stackoverflow.com/questions/46838015/using-api-keys-in-a-react-app
 - https://codeburst.io/react-authentication-with-twitter-google-facebook-and-github-862d59583105
 - https://www.reddit.com/r/webdev/comments/a5hdkk/how_should_i_handle_having_api_keys_in_my_app/
+
+## Videos to the above
+
+- https://www.youtube.com/watch?v=5j4FXMQBzmg
+- https://www.youtube.com/watch?v=Z1ktxiqyiLA
+
+## Links to Node.js tuts
+
+- https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs
+- https://codeburst.io/25-node-js-tutorials-1db3b1da0260
+- https://nodejs.org/en/docs/guides/getting-started-guide/
+
+## Link to Node.js videos
+
+- https://www.youtube.com/watch?v=Z1ktxiqyiLA&list=PLillGF-RfqbbqvbObyE7CldS5-trkefnB
+- https://www.youtube.com/watch?v=k_0ZzvHbNBQ&list=PLillGF-RfqbYRpji8t4SxUkMxfowG4Kqp
+
+## Resources on TypeScript and state
+
+- https://basarat.gitbooks.io/typescript/docs/types/index-signatures.html
+- https://devblog.xero.com/typescript-and-react-whats-react-component-p-s-mean-cfddc65f81e1
+- https://levelup.gitconnected.com/ultimate-react-component-patterns-with-typescript-2-8-82990c516935
--- src/react/components/Form.tsx
+++ src/react/components/Form.tsx
@@ -1,1 +1,54 @@
 import { Component, ReactNode, ReactChild } from 'react';
+
+/* NOTE for integrating with <App/> need to pass a function in as a property like so:
+<Form handleSubmit={this.handleSubmit} /> */
+export class Form extends Component<any, any>{  
+  constructor(props: any) { //https://reactjs.org/docs/react-component.html#constructor
+    super(props); // needs to be called otherwise this.props will be undefined
+
+    this.initialState = {
+        name: '',
+        job: ''
+    };
+
+    this.state = this.initialState;
+  }
+
+  handleChange = (event: any) => { // Analogous to using form.addEventListener('change',handleChange)
+    const {name, value} = event.target;
+
+    this.setState({ //function/ method to set the State of a UI element
+        [name] : value
+    });
+  }
+  submitForm = () => {
+    this.props.handleSubmit(this.state); // submit state of the form with the argument of the values inside this.state, note that we passed the handleSubmit function as a property of the Form component
+    this.setState(this.initialState); // reset form
+  }
+
+  render() {
+    const { name, job } = this.state;
+
+    return (
+        <form>
+            <label>Name</label>
+            <input
+                type="text"
+                name="name"
+                value={name}
+                onChange={this.handleChange} />
+            <label>Job</label>
+            <input
+                type="text"
+                name="job"
+                value={job}
+                onChange={this.handleChange}/>
+            <input
+              type="button"
+              value="Submit"
+              onClick={this.submitForm} />
+        </form>
+    );
+  }
+
+}
